5cd8f76d8c3ce51661a46727f57a86cb0f3cf800
Yenerson H.lopez <74636100+Apock-graficos@users.noreply.github.com>
2023-11-26 14:20:05 -0500

    queso

---
+++ assets/ui/vue-app.js
@@ -0,0 +1,20 @@
+new Vue({
+    el: '#app',
+    data: () => ({
+        title: 'Color palo',
+        navTop: [
+            {
+                id: '#inicio',
+                label: 'Inicio'
+            },
+            {
+                id: '#redes',
+                label: 'Contactame'
+            },
+            {
+                id: '#servicios',
+                label: 'Mis servicios'
+            },
+        ]
+    })
+})
